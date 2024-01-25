f355e2c00b5aea4ad33dc186ba20d216bbbb3133
Yuriy Tarasevich <marten.ao.ns@gmail.com>
2024-01-26 00:40:23 +0300

    added DB to cabin"

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://ijzofhphoesxidtgsapb.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqem9maHBob2VzeGlkdGdzYXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYxODY5NDksImV4cCI6MjAyMTc2Mjk0OX0.g4hB6gmw06bIkLR9_z2_lYGxpkVdkbaCqcsljeWH00g';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
